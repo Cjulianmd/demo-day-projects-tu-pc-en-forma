64f1f63abff9a8fe7bc11a544f80ca3b715325a1
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-09-02 10:52:40 -0500

    PrimerosStyles

---
+++ src/Styles/StylesSebastian.jsx
@@ -0,0 +1,14 @@
+
+
+import styled from 'styled-components';
+
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+
