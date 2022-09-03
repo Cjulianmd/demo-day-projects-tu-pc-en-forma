a4dae985b8a25b3dcaae17057cc43783cb4a55cf
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-09-03 08:44:08 -0500

    UnloggedNavBar

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -3,20 +3,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LandingContainer, LandingImg, MainContainer } from '../../Styles/StylesSebastian';
+import Footer from '../Modules/Footer';
+import UnloggedNavBar from '../Modules/UnloggedNavBar';
 
 function LandingPage() {
 
-  const navigation = useNavigate();
+  // const navigation = useNavigate();
 
-  setTimeout(() => navigation('/login'), 3000);
 
   return (
 
     <MainContainer>
 
-      <LandingContainer>
-        <LandingImg src="https://res.cloudinary.com/dtxqusdhr/image/upload/v1662134335/Demoday/logo_vlilbq.png" alt="Logo de Marca" />
-      </LandingContainer>
+      <UnloggedNavBar />
+
+      <div>Hola</div>
+
+      <Footer />
       
     </MainContainer>
 
