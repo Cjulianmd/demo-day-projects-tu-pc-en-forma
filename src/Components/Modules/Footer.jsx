d28aa625c94d93f1505c3fb8605840716a8f6033
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-09-03 08:36:08 -0500

    Footer

--- src/Components/Modules/Footer.jsx
+++ src/Components/Modules/Footer.jsx
@@ -3,8 +3,6 @@
 
 import React from 'react';
 import { MainContainer } from '../../Styles/StylesSebastian';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 function Footer() {
 
@@ -12,17 +10,15 @@ function Footer() {
 
     <MainContainer>
 
-      {/* <!-- Footer --> */}
       <footer class="text-center text-lg-start bg-light text-muted">
-        {/* <!-- Section: Social media --> */}
+        
         <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          {/* <!-- Left --> */}
+          
           <div class="me-5 d-none d-lg-block">
             <span>Conéctate con nosotros:</span>
           </div>
-          {/* <!-- Left --> */}
+          
 
-          {/* <!-- Right --> */}
           <div>
             <a href="https://www.facebook.com/profile.php?id=100071024058004" class="me-4 text-reset">
               <i class="fab fa-facebook-f"></i>
@@ -40,31 +36,26 @@ function Footer() {
               <i class="fab fa-github"></i>
             </a>
           </div>
-          {/* <!-- Right --> */}
-        </section>
-        {/* <!-- Section: Social media --> */}
 
-        {/* <!-- Section: Links  --> */}
+        </section>
+
         <section class="">
           <div class="container text-center text-md-start mt-5">
-            {/* <!-- Grid row --> */}
+
             <div class="row mt-3">
-              {/* <!-- Grid column --> */}
+
               <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                {/* <!-- Content --> */}
+
                 <h6 class="text-uppercase fw-bold mb-4">
                   <i class="fas fa-gem me-3"></i>Tu PC en Forma
                 </h6>
                 <p>
-                  Here you can use rows and columns to organize your footer content. Lorem ipsum
-                  dolor sit amet, consectetur adipisicing elit.
+                  Somos tu mejor aliado a la hora de tener tu equipo de cómputo en forma para afrontar los mejores retos y puedas ganar por goleada.
                 </p>
               </div>
-              {/* <!-- Grid column --> */}
 
-              {/* <!-- Grid column --> */}
               <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                {/* <!-- Links --> */}
+
                 <h6 class="text-uppercase fw-bold mb-4">
                   Servicios
                 </h6>
@@ -81,30 +72,25 @@ function Footer() {
                   <a href="#!" class="text-reset">Compra de hardware</a>
                 </p>
               </div>
-              {/* <!-- Grid column --> */}
 
-              {/* <!-- Grid column --> */}
               <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                {/* <!-- Links --> */}
+
                 <h6 class="text-uppercase fw-bold mb-4">Contáctanos</h6>
                 <p><i class="fas fa-home me-3"></i> Medellín, Antioquia, Colombia - Bogotá, Cundinamarca, Colombia</p>
                 <p><i class="fas fa-phone me-3"></i> (604) 4480374 </p>
                 <p><i class="fas fa-print me-3"></i> (57) 3207610176 </p>
               </div>
-              {/* <!-- Grid column --> */}
+
             </div>
-            {/* <!-- Grid row --> */}
+
           </div>
         </section>
-        {/* <!-- Section: Links  --> */}
 
-        {/* <!-- Copyright --> */}
         <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
           © 2022 Copyright:
         </div>
-        {/* <!-- Copyright --> */}
+
       </footer>
-      {/* <!-- Footer --> */}
 
     </MainContainer>
 
