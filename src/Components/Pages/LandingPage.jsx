3a256a96431ca364daa0608390ed16b3e4a67bca
Sebastian Aguirre Duque <sadw621@gmail.com>
2022-09-03 14:06:50 -0500

    LandingPage-SecciónTestimonios

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LandingContainer, LandingImg, MainContainer } from '../../Styles/StylesSebastian';
+import { HelperContainer, LandingCard, LandingSections, MainContainer, Polaroid, PolaroidContainer, TestimoniesContainer } from '../../Styles/StylesSebastian';
 import Footer from '../Modules/Footer';
 import UnloggedNavBar from '../Modules/UnloggedNavBar';
 
@@ -19,14 +19,55 @@ function LandingPage() {
 
         <UnloggedNavBar />
 
-        <div>Hola</div>
+        <HelperContainer>
 
+          <LandingSections>
 
+            <TestimoniesContainer>
+
+                <Polaroid>
+                  <img src="https://res.cloudinary.com/dtxqusdhr/image/upload/v1662230712/Demoday/Contador_wkcbrb.jpg" />
+                  <PolaroidContainer>
+                    <h2>Juan</h2>
+                    <h3>Contador</h3>
+                    <p>
+                      «Conocí a <em>Tu PC en Forma</em> por recomendación de una compañera de la universidad, en un día en el cual mi PC sufrió una avería con el disco duro. Ellos me prestaron un equipo durante los dos días que tardaron recuperando la información de mi disco y agregándole uno nuevo al computador; de manera que no tuve que parar mi trabajo ni mis estudios mientras recuperaba mi equipo. Desde entonces, mantengo mi computar en forma de la mano de su calificado equipo, me realizan mantenimientos y actualizaciones sin necesidad de entregar mis equipos, en la mayoría de las veces.»
+                    </p>
+                  </PolaroidContainer>
+                </Polaroid>
+
+                <Polaroid>
+                  <img src="https://res.cloudinary.com/dtxqusdhr/image/upload/v1662230712/Demoday/Contador_wkcbrb.jpg" />
+                  <PolaroidContainer>
+                    <h2>Juan</h2>
+                    <h3>Contador</h3>
+                    <p>
+                      «Conocí a <em>Tu PC en Forma</em> por recomendación de una compañera de la universidad, en un día en el cual mi PC sufrió una avería con el disco duro. Ellos me prestaron un equipo durante los dos días que tardaron recuperando la información de mi disco y agregándole uno nuevo al computador; de manera que no tuve que parar mi trabajo ni mis estudios mientras recuperaba mi equipo. Desde entonces, mantengo mi computar en forma de la mano de su calificado equipo, me realizan mantenimientos y actualizaciones sin necesidad de entregar mis equipos, en la mayoría de las veces.»
+                    </p>
+                  </PolaroidContainer>
+                </Polaroid>
+
+                <Polaroid>
+                  <img src="https://res.cloudinary.com/dtxqusdhr/image/upload/v1662230712/Demoday/Contador_wkcbrb.jpg" />
+                  <PolaroidContainer>
+                    <h2>Juan</h2>
+                    <h3>Contador</h3>
+                    <p>
+                      «Conocí a <em>Tu PC en Forma</em> por recomendación de una compañera de la universidad, en un día en el cual mi PC sufrió una avería con el disco duro. Ellos me prestaron un equipo durante los dos días que tardaron recuperando la información de mi disco y agregándole uno nuevo al computador; de manera que no tuve que parar mi trabajo ni mis estudios mientras recuperaba mi equipo. Desde entonces, mantengo mi computar en forma de la mano de su calificado equipo, me realizan mantenimientos y actualizaciones sin necesidad de entregar mis equipos, en la mayoría de las veces.»
+                    </p>
+                  </PolaroidContainer>
+                </Polaroid>
+
+            </TestimoniesContainer>
+
+          </LandingSections>
+
+        </HelperContainer>
 
       </MainContainer>
 
       <Footer />
-      
+
     </>
 
   )
--- src/Styles/StylesSebastian.jsx
+++ src/Styles/StylesSebastian.jsx
@@ -16,7 +16,7 @@ export const HelperContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 3rem auto;
+  margin: 6rem auto;
 `;
 
 export const LandingImg = styled.img`
@@ -29,4 +29,60 @@ export const LandingContainer = styled.div`
   margin: 7rem auto;
   justify-content: center;
   align-items: center;
+`;
+
+export const LandingSections = styled.div`
+  width: 90vw;
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem;
+`;
+
+export const LandingCard = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem auto;
+`;
+
+export const TestimoniesContainer = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+  justify-content: center;
+  align-items: center;
+  margin: 0.5rem auto;
+`;
+
+export const Polaroid = styled.div`
+  width: 50%;
+  background-color: #9f90ea;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 25px;
+  color: #ffffff;
+
+  img {
+    width: 100%;
+  }
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 700;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+`;
+
+export const PolaroidContainer = styled.div`
+  text-align: center;
+  padding: 10px 20px;
 `;
