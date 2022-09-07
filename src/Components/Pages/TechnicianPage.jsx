1a855548ac79bbee28fc6aab7d1f46d5068359e3
felixcesp <j.f.gcespedes@hotmail.com>
2022-09-06 19:08:39 -0500

    update5

--- src/Components/Pages/TechnicianPage.jsx
+++ src/Components/Pages/TechnicianPage.jsx
@@ -8,19 +8,29 @@ import Footer from "../Modules/Footer";
 import NavBar from "../Modules/NavBar";
 import Table from "react-bootstrap/Table";
 import { GiClick } from "react-icons/gi";
-import { arrayUnion, collection,doc,getDocs, updateDoc } from "firebase/firestore";
+import {BsFillBookmarkCheckFill  } from "react-icons/bs";
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../../Utils/JulianFirebase";
-import '../../Styles/felixCss.css';
+import "../../Styles/felixCss.css";
 import { toast } from "react-toastify";
-
-
-
 
 function TechnicianPage() {
   const [citasL, setCitasL] = useState([]);
+  const [state, setstate] = useState(initialState)
+
 
   useEffect(() => {
-    getDocs(collection(db, "Citas")).then((response) => {
+    getDocs(collection(db, "Citas"), where('estado', '==', 'creada'))
+    .then((response) => {
       let listaCitas = [];
       response.forEach(
         (doc) => {
@@ -31,40 +41,38 @@ function TechnicianPage() {
       setCitasL(listaCitas);
       console.log(listaCitas);
     });
+
+
+    getDocs(query(db, "Tecnicos", '1230'))
+    .then((resp) => {
+        setReserva(resp)     
+      });
+
   }, [setCitasL]);
 
-  const escogerCita=(  
-    name, 
-    apellidos,
-    DNI,
-    equipo,
-    telefono,
-    correo,
-    descripcion,
-    fecha,
-    hora
-    )=>{
-     updateDoc(doc(db, 'Tecnicos', '1230'),{
-        citas: arrayUnion({
-            
-            name: name,
-            apellidos:apellidos,
-            DNI:DNI,
-            equipo:equipo,
-            telefono:telefono,
-            correo:correo,
-            descripcion:descripcion,
-            fecha:fecha,
-            hora:hora
-            
-        })
-      
-     })
-     .then(()=>{
-        toast.success('Cita agregada. ');
-     })
 
-  }
+
+
+
+
+  const escogerCita = (index
+  
+  ) => {
+    updateDoc(doc(db, "Tecnicos", "1230"), {
+      citas: arrayUnion(
+        citasL[index].DNI
+      ),
+    }).then(() => {
+        updateDoc(doc(db, "Citas", citasL[index].DNI ), {estado:'agendada', tecnico: 'name'})
+        .then(()=>{
+            let aux = Object.assign([], citasL)
+            aux.splice(index, 1)
+            setCitasL(aux)
+            toast.success("Cita agregada. ");
+        })
+   
+    });
+  };
 
   return (
     <>
@@ -73,8 +81,16 @@ function TechnicianPage() {
         <HelperContainer>
           <LandingSections>
             <h1>Citas disponibles.</h1>
-            <p style={{margin:'1.5rem 0' }}>Estas son las citas disponibles en el sistema. Elige una:</p>
-            <Table striped bordered hover size="sm" className="react-strap-table">
+            <p style={{ margin: "1.5rem 0" }}>
+              Estas son las citas disponibles en el sistema. Elige una:
+            </p>
+            <Table
+              striped
+              bordered
+              hover
+              size="sm"
+              className="react-strap-table"
+            >
               <thead>
                 <tr>
                   <th>#</th>
@@ -105,17 +121,18 @@ function TechnicianPage() {
                       <td>{i.fecha}</td>
                       <td>{i.hora}</td>
                       <td>
-                        <GiClick style={{cursor:'pointer', fontSize:'1.5rem', color:'#675cb0'}} onClick={()=> escogerCita(
-                            i.name, 
-                            i.apellidos,
-                            i.DNI,
-                            i.equipo,
-                            i.telefono,
-                            i.correo,
-                            i.descripcion,
-                            i.fecha,
-                            i.hora
-                            )}/>
+                        <GiClick
+                          style={{
+                            cursor: "pointer",
+                            fontSize: "1.5rem",
+                            color: "#675cb0",
+                          }}
+                          onClick={() =>
+                            escogerCita(
+                            index
+                            )
+                          }
+                        />
                       </td>
                     </tr>
                   );
@@ -127,19 +144,38 @@ function TechnicianPage() {
             <Table striped bordered hover size="sm">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>Cita</th>
+                  <th>Nombre</th>
+                  <th>Apellidos</th>
+                  <th>DNI</th>
+                  <th>Equipo</th>
+                  <th>Teléfono</th>
+                  <th>Correo</th>
+                  <th>Descripción</th>
+                  <th>Fecha</th>
+                  <th>Hora</th>
+                  <th>Completado</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
+               {reserva.citas.map((c, index, citas) => {
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{c.name}</td>
+                      <td>{c.apellidos}</td>
+                      <td>{c.DNI}</td>
+                      <td>{c.equipo}</td>
+                      <td>{c.telefono}</td>
+                      <td>{c.correo}</td>
+                      <td>{c.descripcion}</td>
+                      <td>{c.fecha}</td>
+                      <td>{c.hora}</td>
+                      <td><BsFillBookmarkCheckFill/></td>
+                    </tr>
+                  );
+                })}
+
                 <tr>
                   <td>2</td>
                   <td>Jacob</td>
